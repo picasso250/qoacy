f2fa382aad0bc2d1bc5f7eabfe542618415a77dc
picasso250 <cumt.xiaochi@gmail.com>
2013-10-10 13:04:57 +0800

    add and answer action

--- source/controller/QuestionController.php
+++ source/controller/QuestionController.php
@@ -5,11 +5,34 @@
 
 class QuestionController extends Controller
 {
-    function viewAction()
+    public function viewAction()
     {
         $question = $this->questionDao->findOne($this->id);
         $answers = $question->getAnswers();
         $this->addScripts('jquery.form');
         $this->renderView('master', compact('question', 'answers'));
+    }
+
+    public function askAction()
+    {
+        $title = $this->param('title');
+        $question = $this->questionDao
+                ->add(array(
+                    'title' => $title,
+                    'user_id' => $this->currentUser->id;
+                ));
+        $this->redirect("/question/$question->id");
+    }
+
+    public function answerAction()
+    {
+        $content = $this->param('content');
+        $this->answerDao
+            ->add(array(
+                'question_id' => $this->param('question_id'),
+                'content' => $content,
+                'user_id' => $this->currentUser->id,
+            ));
+        return $this->renderBlock('question/answer');
     }
 }
