31be3e3764d68a327f387fefb04b1b2fb9634171
picasso250 <cumt.xiaochi@gmail.com>
2013-10-12 13:02:04 +0800

    login

---
+++ source/controller/AccountDao.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author  ryan <cumt.xiaochi@gmail.com>
+ */
+
+class AccountDao extends IdDao
+{
+	
+	function login_init()
+	{
+		$GLOBALS['data'] = array();
+	}
+
+	function login_POST()
+	{
+		$username = _post('username');
+		$password = _post('password');
+		if ($user = User::check($username, $password)) {
+			$user->login();
+			redirect();
+		} else {
+			$msg = 'wrong user name or password';
+			$GLOBALS['data']['msg'] = $msg;
+			$GLOBALS['data']['username'] = $username;
+		}
+	}
+
+	function login_end()
+	{
+		add_scripts('jquery.validate.min');
+		render_view('master', $GLOBALS['data']);
+	}
+}
