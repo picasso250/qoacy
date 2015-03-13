3d06f1b083a0e46219e51ce3ad5931d62b30dd22
wangxiaochi <wangxiaochi@baidu.com>
2015-03-13 17:10:08 +0800

    login and register

--- js/common.js
+++ js/common.js
@@ -1,9 +1,19 @@
 $(function () {
-    $('.post-action-btn').click(function () {
-        var action = $(this).data('action');
-        var backurl = $(this).data('backurl');
-        $.post(action, {}, function (ret) {
-            window.location.href = backurl;
-        });
-    });
+	var alert = $('.alert');
+	var postForm = $('form[method="post"][role=post]').on('submit', function (e) {
+		e.preventDefault();
+		var $this = $(this);
+		var $btn = $(this).find('button[type=submit]');
+		$btn.button('loading');
+		$.post($this.attr('action'), $this.serialize(), function (ret) {
+			if (ret.code === 0) {
+				if (ret.data && ret.data.url) {
+					location.href = ret.data.url;
+					return;
+				}
+			}
+			$btn.button('reset');
+			alert.removeClass('alert-hidden').text(ret.message);
+		}, 'json');
+	});
 });
--- source/actions.php
+++ source/actions.php
@@ -4,8 +4,56 @@ function index_GET()
     $questions = Service('db')->get_all_question(100);
     render(VIEW_ROOT.'/index.html', compact('questions'), LAYOUT_PATH);
 }
+function login_ajax()
+{
+	list($ok, $msg) = user_login();
+	if ($ok) {
+		echo json(['url' => _get('back', '/')]);
+	} else {
+		echo json(1, $msg);
+	}
+}
 function login()
 {
     render(VIEW_ROOT.'/login.html', compact('questions'), LAYOUT_PATH);
+}
 
+function register_ajax()
+{
+	$message = '';
+	$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+	$password = _post('password');
+	if (empty($email)) {
+		$message = '邮箱不正确';
+	} elseif (empty($password)) {
+		$message = '密码必填';
+	}
+	if ($message) {
+		return [false, $message];
+	}
+	$db = Service('db');
+	$db->beginTransaction();
+	try {
+		$user = $db->get_user_by_email($email);
+		if ($user) {
+			echo json(1, '此用户已经存在');
+			return;
+		}
+		error_log("$email register, $password");
+		$id = $db->insert('user', [
+			'email' => $email,
+			'password' => md5($password),
+		]);
+		user_id($id);
+		$db->commit();
+		echo json(['url' => '/']);
+	} catch (Exception $e) {
+		$db->rollback();
+		throw $e;
+	}
+}
+
+function register()
+{
+	render(VIEW_ROOT.'/register.html', [], LAYOUT_PATH);
 }
