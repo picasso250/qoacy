f4d9651669188248f6903881ad101ee9980e82bf
wangxiaochi <wangxiaochi@baidu.com>
2015-03-13 16:33:01 +0800

    发

--- source/actions.php
+++ source/actions.php
@@ -4,3 +4,8 @@ function index_GET()
     $questions = Service('db')->get_all_question(100);
     render(VIEW_ROOT.'/index.html', compact('questions'), LAYOUT_PATH);
 }
+function login()
+{
+    render(VIEW_ROOT.'/login.html', compact('questions'), LAYOUT_PATH);
+
+}
